a903ae715781d805f6640844be0e55b6bc96e7a0
eavan <webwwy@gmail.com>
2024-03-16 00:30:21 +0800

    1

---
+++ src/app/(main)/Header.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Container } from "@/components/ui/Container"
+
+export function Header () {
+  return (
+    <header>
+      <Container>
+        <div className="flex">
+          <div>logo</div>
+          <div>logo</div>
+          <div>logo</div>
+        </div>
+      </Container>
+    </header>
+  )
+}
