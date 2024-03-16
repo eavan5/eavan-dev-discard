7193a9bd53abc8e9ed25ebc2cbce3ad1db1f089c
eavan <webwwy@gmail.com>
2024-03-16 14:08:55 +0800

    feat: header

--- src/app/(main)/Header.tsx
+++ src/app/(main)/Header.tsx
@@ -12,23 +12,53 @@ export function Header () {
           <div className="text-2xl font-semibold">
             Eavan
           </div>
-          <nav>
-            <ul className="flex space-x-10 text-lg font-medium">
-              <li>
-                <a href="/">主页</a>
-              </li>
-              <li>
-                <a href="/about">运动</a>
-              </li>
-              <li>
-                <a href="/blog">阅读</a>
-              </li>
-              <li>
-                <a href="/contact">联系</a>
-              </li>
-            </ul>
+          <div>
+            <div className="sm:hidden">
+              <label htmlFor="Tab" className="sr-only">Tab</label>
 
-          </nav>
+              <select
+                id="Tab"
+                className="w-full rounded-md border-gray-200 dark:border-gray-700 dark:bg-gray-900 dark:text-white"
+              >
+                <option selected>Settings</option>
+                <option>Messages</option>
+                <option>Archive</option>
+                <option >Notifications</option>
+              </select>
+            </div>
+
+            <div className="hidden sm:block">
+              <nav className="flex gap-6" aria-label="Tabs">
+                <a
+                  href="#"
+                  className="shrink-0 rounded-lg p-2 text-sm font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-900 dark:hover:text-gray-200"
+                >
+                  主页
+                </a>
+
+                <a
+                  href="#"
+                  className="shrink-0 rounded-lg p-2 text-sm font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-900 dark:hover:text-gray-200"
+                >
+                  运动
+                </a>
+
+                <a
+                  href="#"
+                  className="shrink-0 rounded-lg p-2 text-sm font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-900 dark:hover:text-gray-200"
+                >
+                  阅读
+                </a>
+                <a
+                  href="#"
+                  className="shrink-0 rounded-lg bg-gray-100 p-2 text-sm font-medium text-gray-700 dark:bg-gray-800 dark:text-gray-200"
+                  aria-current="page"
+                >
+                  关于
+                </a>
+              </nav>
+            </div>
+          </div>
           <div>
             {/* @ts-ignore */}
             <Around duration={750} className="scale-150" />
